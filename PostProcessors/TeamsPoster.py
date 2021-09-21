0e60c82999fddb5c8d33a1e10c49e325ff66cfe9
Ryon Riley <52056485+atechnextdoor@users.noreply.github.com>
2021-09-21 13:33:42 -0400

    Update TeamsPoster.py

--- PostProcessors/TeamsPoster.py
+++ PostProcessors/TeamsPoster.py
@@ -103,7 +103,7 @@ class TeamsPoster(Processor):
                 """ % (name, version, catalog, pkg_path, pkginfo_path)
                 
                 response = requests.post(
-                webhook_url, json=teams_data).json()
+                webhook_url, json=teams_data)
                 if response.status_code != 200:
                     raise ValueError(
                                 'Request to Teams returned an error %s, the response is:\n%s'
